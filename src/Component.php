eee20fb35b5abe2773d3a9940fbc8b35d8880aba
Leonardo Losoviz <leo@getpop.org>
2020-11-17 23:40:13 +0800

    Added dependencies

--- src/Component.php
+++ src/Component.php
@@ -20,7 +20,46 @@ class Component extends AbstractComponent
     public static function getDependedComponentClasses(): array
     {
         return [
-            \PoPSitesWassup\CustomPostMutations\Component::class,
+            \GraphQLByPoP\GraphQLServer\Component::class,
+            \Leoloso\ExamplesForPoP\Component::class,
+            \PoP\FunctionFields\Component::class,
+            \PoP\RESTAPI\Component::class,
+            \PoP\TraceTools\Component::class,
+
+            \PoP\SiteWP\Component::class,
+            \PoP\SPA\Component::class,
+
+            \PoPSchema\BlockMetadataWP\Component::class,
+            \PoPSchema\CDNDirective\Component::class,
+            \PoPSchema\CommentMetaWP\Component::class,
+            \PoPSchema\ConvertCaseDirectives\Component::class,
+            \PoPSchema\CustomPostMediaWP\Component::class,
+            \PoPSchema\CustomPostMetaWP\Component::class,
+            \PoPSchema\GoogleTranslateDirectiveForCustomPosts\Component::class,
+            \PoPSchema\PagesWP\Component::class,
+            \PoPSchema\PostsWP\Component::class,
+            \PoPSchema\PostTagsWP\Component::class,
+            \PoPSchema\TaxonomyQueryWP\Component::class,
+            \PoPSchema\UserMetaWP\Component::class,
+            \PoPSchema\UserRolesACL\Component::class,
+            \PoPSchema\UserRolesWP\Component::class,
+            \PoPSchema\UserStateWP\Component::class,
+            \PoPSchema\PostMutations\Component::class,
+            \PoPSchema\CustomPostMediaMutationsWP\Component::class,
+            \PoPSchema\CommentMutationsWP\Component::class,
+
+            \PoPSchema\CategoriesWP\Component::class,
+            \PoPSchema\NotificationsWP\Component::class,
+            \PoPSchema\MenusWP\Component::class,
+            \PoPSchema\EventMutationsWPEM\Component::class,
+            \PoPSchema\EverythingElseWP\Component::class,
+            \PoPSchema\HighlightsWP\Component::class,
+            \PoPSchema\LocationPostsWP\Component::class,
+            \PoPSchema\StancesWP\Component::class,
+
+            \PoPSitesWassup\PostMutations\Component::class,
+            \PoPSitesWassup\HighlightMutations\Component::class,
+            \PoPSitesWassup\StanceMutations\Component::class,
         ];
     }
 
